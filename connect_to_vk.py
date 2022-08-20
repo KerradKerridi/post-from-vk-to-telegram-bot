5e637d04d6b9d2e2fafffee56f416330b6e5ab5f
KerradKerridi <KerradKerridi>
2022-08-20 15:49:13 +0300

    captcha added

--- connect_to_vk.py
+++ connect_to_vk.py
@@ -11,9 +11,29 @@ LOGIN = config.get('VK', 'LOGIN')
 PASSWORD = config.get('VK', 'PASSWORD')
 API_TOKEN = config.get('VK', 'TOKEN', fallback=None)
 
-vk_session = vk_api.VkApi(LOGIN, PASSWORD, API_TOKEN)
+def auth_handler():
+    """ При двухфакторной аутентификации вызывается эта функция.
+    """
 
+    # Код двухфакторной аутентификации
+    key = input("Enter authentication code: ")
+    # Если: True - сохранить, False - не сохранять.
+    remember_device = True
 
+    return key, remember_device
+
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
+
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
+
+vk_session = vk_api.VkApi(LOGIN, PASSWORD, API_TOKEN, auth_handler=auth_handler, captcha_handler=captcha_handler)
 # Получаем данные о посте пользователя из vk.com
 def get_data(domain_vk, count_vk):
     try:
